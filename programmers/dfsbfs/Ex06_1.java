2a5fd9a6a87cce4e80dc57811255e2de82df7f7b
ssm00 <titan0724@mju.ac.kr>
2024-06-09 16:43:05 -0600

    Update Ex06_1.java

--- programmers/dfsbfs/Ex06_1.java
+++ programmers/dfsbfs/Ex06_1.java
@@ -1,6 +1,7 @@
 package dfsbfs;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ex06_1 {
 
@@ -9,11 +10,12 @@ public class Ex06_1 {
     static boolean visited[];
 
     public String[] solution(String[][] tickets) {
-        boolean[] visited = new boolean[tickets.length];
-
-
-
-
+        visited = new boolean[tickets.length];
+        dfs(0, "ICN", "ICN ", tickets);
+        String[] answer = {};
+        Collections.sort(list);
+        answer = list.get(0).split(" ");
+        return answer;
     }
 
     //나의 사고 -> String arraylist에 now 포함되어있다
@@ -24,8 +26,10 @@ public class Ex06_1 {
             return;
         }
         for (int i = 0; i < visited.length; i++) {
-            if (visited[i] != true && now.equals(tickets[i][1])) {
-                
+            if (visited[i] != true && now.equals(tickets[i][0])) {
+                visited[i] = true;
+                dfs(depth + 1, tickets[i][1], path + tickets[i][1]+" ", tickets);
+                visited[i] = false;
             }
         }
     }
@@ -36,7 +40,7 @@ public class Ex06_1 {
         String a[][] = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
         String b[][] = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
         Ex06_1 ex061 = new Ex06_1();
-        String[] solution = ex061.solution(a);
+        String[] solution = ex061.solution(b);
         System.out.println(solution.length);
         for (int i = 0; i < solution.length; i++) {
             System.out.println(solution[i]);
